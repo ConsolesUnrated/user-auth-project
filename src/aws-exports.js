c2dfe002f95b4b4cd29b5cb3537f3add04e2b503
James <consolesunrated@gmail.com>
2025-03-31 13:59:05 -0700

    Fix user pool ID

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,6 +1,6 @@
 export default {
     aws_project_region: "us-west-2",
     aws_cognito_region: "us-west-2",
-    aws_user_pools_id: "us-west-2_9zmfr0ly3",
+    aws_user_pools_id: "us-west-2_8zto5iRxB",
     aws_user_pools_web_client_id: "75u7n3v9ii3tovg4htudb50938",
 };
